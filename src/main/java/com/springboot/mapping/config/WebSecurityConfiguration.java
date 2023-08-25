701a2020eab2ae49d613a9d9c2735e8d8f081ad4
Sami1150 <msamiahmed572@gmail.com>
2023-08-25 17:33:57 +0500

    Testing with SB Security

--- src/main/java/com/springboot/mapping/config/WebSecurityConfiguration.java
+++ src/main/java/com/springboot/mapping/config/WebSecurityConfiguration.java
@@ -6,11 +6,13 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
 @EnableMethodSecurity
 @Configuration
 public class WebSecurityConfiguration {
@@ -26,25 +28,58 @@ public class WebSecurityConfiguration {
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
+
+        /*
+        * void customize(WebSecurity web)
+        Performs the customizations on WebSecurity.
+        Parameters:
+            web - the instance of WebSecurity to apply to customizations to
+        @Bean
+        public WebSecurityCustomizer ignoringCustomizer()
+             return (web) -> web.ignoring().requestMatchers("/ignore1", "/ignore2");
+
+        *** GPT ***
+            * Configure custom exception handling for authentication and authorization failures.
+            * Add or modify security filters in the request processing chain.
+            * Define specific URL patterns that should be ignored or allowed without authentication.
+            * Customize headers, CSRF protection, or other security-related settings.
+        *** ***
+        */
+
 //        return web -> web.ignoring().requestMatchers(ignored).requestMatchers(HttpMethod.GET, ignoredGet);
         return web -> web.ignoring().requestMatchers(
-                new AntPathRequestMatcher("/api/v1/teacher", "GET"),
-                new AntPathRequestMatcher("/h2-console/**", "GET"), // Allow GET requests to h2-console
-                new AntPathRequestMatcher("/h2-console/**", "POST")
+//                new AntPathRequestMatcher("/api/v1/teacher", "GET"),
+//                new AntPathRequestMatcher("/api/v1/teacher","POST"),
+//                new AntPathRequestMatcher("/h2-console/**", "GET"), // Allow GET requests to h2-console
+//                new AntPathRequestMatcher("/h2-console/**", "POST"),
+                new AntPathRequestMatcher("/actuator", "GET")
+//                ,new AntPathRequestMatcher("/actuator/**","GET")
         );
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        /*Enable and customize  form based authentication
+
+          Form-based authentication is a common way to authenticate users by presenting them with a login form where they can enter their credentials.
+
+         */
         http.formLogin(Customizer.withDefaults());
+
 //        http.exceptionHandling(config -> config.defaultAuthenticationEntryPointFor(
 //                (request, response, ex) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Not Authorized"),
 //                AntPathRequestMatcher.antMatcher(api)));
-        http.csrf(config -> config.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()));
+
+//        http.csrf(config -> config.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()));
 
         http.authorizeHttpRequests(config -> config.anyRequest().authenticated());
-        http.csrf(csrf -> csrf.disable());
+
+//        Disable CSRF
+//        http.csrf(csrf -> csrf.disable());
+
+
+
         return http.build();
     }
 }
--- src/test/java/com/springboot/mapping/teacher/TeacherControllerTest.java
+++ src/test/java/com/springboot/mapping/teacher/TeacherControllerTest.java
@@ -5,178 +5,132 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TeacherControllerTest {
     @Autowired
     private MockMvc mockMvc;
-//    @Test
-//    public void testFindAllTeachers() throws Exception {
-//        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/teacher"))
-//                .andDo(MockMvcResultHandlers.print())
-//                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
-//                .andExpect(MockMvcResultMatchers.content().string(
-//                        Matchers.startsWith("{\"content\":[{\"id\":1,\"course\":\"Science\",\"salary\":50000},{\"id\":2,\"course\":\"Math\",\"salary\":60000},{\"id\":3,\"course\":\"History\",\"salary\":45000}]}"
-//                        )));
-//    }
-//    @Test
-//    public void testFindAll() {
-//        // Mock the TeacherService
-//        TeacherService teacherService = mock(TeacherService.class);
-//
-//        // Create sample data
-//        Teacher teacher1 = new Teacher(1, "Science", 50000);
-//        Teacher teacher2 = new Teacher(2, "Math", 60000);
-//        Teacher teacher3 = new Teacher(3, "History", 45000);
-//        List<Teacher> teachers = Arrays.asList(teacher1, teacher2, teacher3);
-//
-//        // Mock the service method
-//        when(teacherService.findAll()).thenReturn(teachers);
-//
-//        // Create the controller
-//        TeacherController teacherController = new TeacherController(teacherService);
-//
-//        // Test the controller method
-//        ResponseEntity<Map<String, List<Teacher>>> response = teacherController.findAll();
-//
-//        // Assert the response status code
-//        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
-//
-//        // Assert the response body
-//        Map<String, List<Teacher>> responseBody = response.getBody();
-//        assertThat(responseBody, notNullValue());
-//        assertThat(responseBody.get("content"), equalTo(teachers));
-//    }
-//
-//    // Test Get Teacher by ID:
-//    @Test
-//    public void testFindById() {
-//        // Mock the TeacherService
-//        TeacherService teacherService = mock(TeacherService.class);
-//
-//        // Create a sample teacher
-//        Teacher teacher = new Teacher(1, "Science", 50000);
-//
-//        // Mock the service method
-//        when(teacherService.findById(1L)).thenReturn(teacher);
-//        when(teacherService.findById(999L)).thenReturn(null); // Non-existent teacher
-//
-//        // Create the controller
-//        TeacherController teacherController = new TeacherController(teacherService);
-//
-//        // Test the controller method for existing teacher
-//        ResponseEntity<Teacher> responseExisting = teacherController.findById(1L);
-//        assertThat(responseExisting.getStatusCode(), equalTo(HttpStatus.OK));
-//        assertThat(responseExisting.getBody(), equalTo(teacher));
-//
-//        // Test the controller method for non-existent teacher
-//        ResponseEntity<Teacher> responseNonExistent = teacherController.findById(999L);
-//        assertThat(responseNonExistent.getStatusCode(), equalTo(HttpStatus.NOT_FOUND));
-//    }
-//
-//
-//    // Test to Verify Post Method
-//    @Test
-//    public void testPost() {
-//        // Mock the TeacherService and Repository
-//        TeacherService teacherService = mock(TeacherService.class);
-//        TeacherRepository teacherRepository = mock(TeacherRepository.class);
-//
-//        // Create a sample teacher
-//        Teacher newTeacher = new Teacher(1, "Math", 60000);
-//
-//        // Mock the service method
-//        when(teacherService.create(any(Teacher.class))).thenReturn(newTeacher);
-//
-//        // Create the controller
-//        TeacherController teacherController = new TeacherController(teacherService);
-//
-//        // Test the controller method
-//        ResponseEntity<Teacher> response = teacherController.create(newTeacher);
-//
-//        // Assert the response status code and body
-//        verify(teacherService).create(newTeacher);
-//        assertEquals(HttpStatus.OK, response.getStatusCode());
-//        assertEquals(newTeacher, response.getBody());
-//    }
-//
-//
-//    // Test to verify PUT or UPDATE Method
-//    @Test
-//    public void testUpdate() {
-//        // Mock the TeacherService and Repository
-//        TeacherService teacherService = mock(TeacherService.class);
-//        TeacherRepository teacherRepository = mock(TeacherRepository.class);
-//
-//        // Create a sample teacher
-//        Teacher existingTeacher = new Teacher(1, "Science", 50000);
-//        Teacher updatedTeacher = new Teacher(1, "Math", 60000);
-//
-//        // Mock the service method
-//        when(teacherService.update(eq(1L), any(Teacher.class))).thenReturn(updatedTeacher);
-//
-//        // Create the controller
-//        TeacherController teacherController = new TeacherController(teacherService);
-//
-//        // Test the controller method
-//        ResponseEntity<Teacher> response = teacherController.update(1L, updatedTeacher);
-//
-//        // Assert the response status code and body
-//        verify(teacherService).update(eq(1L), any(Teacher.class));
-//        assertEquals(HttpStatus.OK, response.getStatusCode());
-//        assertEquals(updatedTeacher, response.getBody());
-//    }
-//
-//    // Test to verify DELETE Method
-//    @Test
-//    public void testDelete() {
-//        // Mock the TeacherService and Repository
-//        TeacherService teacherService = mock(TeacherService.class);
-//        TeacherRepository teacherRepository = mock(TeacherRepository.class);
-//
-//        // Create a sample teacher
-//        Teacher existingTeacher = new Teacher(1, "Science", 50000);
-//
-//        // Mock the service method
-//        when(teacherService.delete(eq(1L))).thenReturn(true);
-//
-//        // Create the controller
-//        TeacherController teacherController = new TeacherController(teacherService);
-//
-//        // Test the controller method for successful deletion
-//        ResponseEntity<String> responseSuccess = teacherController.delete(1L);
-//
-//        // Assert the response status code and body
-//        verify(teacherService).delete(eq(1L));
-//        assertEquals(HttpStatus.OK, responseSuccess.getStatusCode());
-//        assertEquals("Resource deleted successfully", responseSuccess.getBody());
-//
-//        // Mock the service method for unsuccessful deletion
-//        when(teacherService.delete(eq(2L))).thenReturn(false);
-//
-//        // Test the controller method for unsuccessful deletion
-//        ResponseEntity<String> responseFailure = teacherController.delete(2L);
-//
-//        // Assert the response status code and body
-//        verify(teacherService).delete(eq(2L));
-//        assertEquals(HttpStatus.NOT_FOUND, responseFailure.getStatusCode());
-//        assertEquals("Resource not found or could not be deleted", responseFailure.getBody());
-//    }
+    @MockBean
+    private TeacherService teacherService;
+    @Test
+    public void testFindAllTeachers() throws Exception {
+        mockMvc.perform(get("/api/v1/teacher")
+                        .with(testUser("reporter","REPORTER"))
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.content().string(
+                        Matchers.startsWith("{\"content\":[{\"id\":1,\"course\":\"Science\",\"salary\":50000},{\"id\":2,\"course\":\"Math\",\"salary\":60000},{\"id\":3,\"course\":\"History\",\"salary\":45000}]}"
+                        )));
+    }
+
+    // Test Get Teacher by ID:
+    @Test
+    public void testFindById() throws Exception {
+        // Create a sample teacher
+        Teacher teacher = new Teacher(1, "Science", 50000);
+
+        // Mock the service method
+        when(teacherService.findById(1L)).thenReturn(teacher);
+        when(teacherService.findById(999L)).thenReturn(null); // Non-existent teacher
+
+        // Test the controller method for existing teacher
+        mockMvc.perform(get("/api/v1/teacher/1")
+                        .with(testUser("reporter","REPORTER"))
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", equalTo(1)))
+                .andExpect(jsonPath("$.course", equalTo("Science")))
+                .andExpect(jsonPath("$.salary", equalTo(50000)))
+                .andReturn();
+
+        // Test the controller method for non-existent teacher
+        mockMvc.perform(get("/api/v1/teacher/999")
+                .with(testUser("reporter","REPORTER"))
+                .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
+
+
+    // Test to Verify Post Method
+    @Test
+    public void testPost() throws Exception {
+        Teacher newTeacher = new Teacher(4, "Math", 60000); // Creating a new teacher object
+
+        // Mock the service method
+        when(teacherService.create(any(Teacher.class))).thenReturn(newTeacher);
+
+        // Test the controller method for creating a new teacher
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/teacher/")
+                        .with(testUser("reporter","REPORTER"))
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .contentType("application/json")
+                        .content("{\"id\": 4, \"course\": \"Math\", \"salary\": 60000}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", equalTo(4)))
+                .andExpect(jsonPath("$.course", equalTo("Math")))
+                .andExpect(jsonPath("$.salary", equalTo(60000)))
+                .andReturn();
+    }
+
+
+
+    // Test to verify PUT or UPDATE Method
+    @Test
+    public void testUpdate() throws Exception {
+        Teacher updatedTeacher = new Teacher(4, "Biology", 55000); // Creating an updated teacher object
+
+        // Mock the service method
+        when(teacherService.update(eq(4L), any(Teacher.class))).thenReturn(updatedTeacher);
+
+        // Test the controller method for updating a teacher
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/teacher/4/")
+                        .with(testUser("reporter", "REPORTER"))
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .contentType("application/json")
+                        .content("{\"id\": 4, \"course\": \"Biology\", \"salary\": 55000}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", equalTo(4)))
+                .andExpect(jsonPath("$.course", equalTo("Biology")))
+                .andExpect(jsonPath("$.salary", equalTo(55000)))
+                .andReturn();
+    }
+
+    // Test to verify DELETE Method
+    @Test
+    public void testDelete() throws Exception {
+        // Mock the service method to indicate successful deletion
+        when(teacherService.delete(4L)).thenReturn(true);
+
+        // Test the controller method for deleting a teacher
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/teacher/4")
+                        .with(testUser("reporter", "REPORTER"))
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(status().isNoContent()) // HTTP 204 indicates successful deletion
+                .andReturn();
+    }
+
+
+    private RequestPostProcessor testUser(String userName, String authoriy) {
+        return SecurityMockMvcRequestPostProcessors.user(userName).authorities(new SimpleGrantedAuthority(authoriy));
+    }
 }
